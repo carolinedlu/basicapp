eb5063ddf8abccbe334bb7ff291ef6483c61c7a8
hargun3045 <hargun3045@gmail.com>
2021-10-05 16:13:10 +0530

    remove plotly

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import info
-import plotly as po
-import plotly.express as px
-import plotly.graph_objects as go
 
 st.set_page_config(page_title="Analytics",layout='wide')
 
